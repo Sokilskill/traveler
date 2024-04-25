8180192ee27ae106cb63502867126c1de18ce188
Sokilskill <vprovorotov@gmail.com>
2024-04-25 13:03:16 +0300

    events @click

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,16 +1,9 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import { computed, ref } from 'vue'
-const counter = ref(0) // { value: 0 }
-const user = ref({ name: 'Jack', age: 10 })
+import { ref } from 'vue'
 
-const increment = () => {
-  counter.value += 1
-}
-const decrement = () => {
-  counter.value -= 1
-}
+const user = ref({ name: 'Jack', age: 10 })
 
 const newUserName = ref('')
 
@@ -18,20 +11,6 @@ const updateName = () => {
   user.value.name = newUserName.value
   newUserName.value = ''
 }
-
-const disabledColor = computed(() => {
-  if (counter.value <= 2) {
-    return 'bg-red-200'
-  } else if (counter.value <= 4) {
-    return 'bg-red-400'
-  } else if (counter.value <= 6) {
-    return 'bg-red-500'
-  } else if (counter.value <= 8) {
-    return 'bg-red-700'
-  }else {
-    return 'bg-orange-950'
-  }
-})
 </script>
 
 <template>
@@ -40,19 +19,12 @@ const disabledColor = computed(() => {
     <FavoritePlace :key="n" v-for="n in 4" />
 
     <p>{{ user.name }}</p>
-    <div :class="disabledColor" >{{ disabledColor }}</div>
+
     <input placeholder="Change name" v-model="newUserName" @keyup.enter="updateName" />
 
-    <p class="text-center text-2xl">Counter: {{ counter }}</p>
+    <button @click="(()=> console.log('click3'))" @keydown.space.a="()=> console.log('click == a')">click me</button>
 
-    <IButton
-      :class="disabledColor"
-      class="w-full mt-5"
-      @click="increment"
-      :disabled="counter === 10"
-      >Додати</IButton
-    >
-    <IButton class="w-full mt-5" @click="decrement" :disabled="counter === 0">Відняти</IButton>
+    <a href="/" class="block text-green-600" @click.prevent="()=> console.log('no refresh')" >link without refresh page</a>
 
     <IButton class="w-full mt-10">Додати маркер</IButton>
   </div>
