be838b3307b7c27a8a373f8e686648ac2653eb2c
Sokilskill <vprovorotov@gmail.com>
2024-04-26 17:32:36 +0300

    add Form Container

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,31 +1,19 @@
 <script setup>
 import IButton from '@/components/IButton/IButton.vue'
 import IInput from '@/components/IInput/IInput.vue'
-import { reactive, ref, toRaw } from 'vue'
+import { reactive, toRaw } from 'vue'
+import FormContainer from '../FormContainer.vue'
 
-const emit = defineEmits(['submit'])
+defineEmits(['submit'])
 const userData = reactive({
   name: '',
   email: '',
   password: ''
 })
-
-// const userData = ref({
-//   name: '',
-//   email: '',
-//   password: ''
-// })
 </script>
 
 <template>
-  <!-- @submit.prevent="emit('submit', userData)"  якщо використовувати reactive поверне Proxy 
-        @submit.prevent="emit('submit', toRaw(userData))"  якщо потрібно повертати об'єкт треба додати toRaw
-  -->
-
-  <form
-    @submit.prevent="emit('submit', toRaw(userData))"
-    class="max-w-[500px] w-full p-10 bg-white rounded-2xl"
-  >
+  <FormContainer @submit.prevent="$emit('submit', toRaw(userData))">
     <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
     <IInput
       class="mb-4"
@@ -37,5 +25,5 @@ const userData = reactive({
     <IInput label="Пароль" type="password" v-model="userData.password" />
 
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Створити аккаунт</IButton>
-  </form>
+  </FormContainer>
 </template>
