c8415b6fec2b606d8fc8d2683070aae1e0bb70de
Sokilskill <vprovorotov@gmail.com>
2024-04-29 11:11:15 +0300

    create modal with lifecycle

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 // import HomepageView from './views/HomepageView.vue'
 // import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import IModal from './components/IModal/IModal.vue'
+
+const isOpen = ref(false)
+const closeModal = () => {
+  isOpen.value = false
+}
+const openModal = () => {
+  isOpen.value = true
+}
 </script>
 
 <template>
@@ -11,7 +21,8 @@ import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
   <!-- <div class="bg-white h-screen w-[400px]">
     <FavoritePlaces />
   </div> -->
-
+  <button class="text-red-500 bg-amber-400 px-5 rounded-lg" @click="openModal">Open modal</button>
   <RegistrationForm @submit="(e) => console.log(e)" />
   <LoginForm @submit="console.log" />
+  <IModal v-if="isOpen" @close="closeModal">Hello Modal</IModal>
 </template>
---
+++ src/components/IModal/IModal.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+import CrossIcon from '../icons/CrossIcon.vue'
+const emit = defineEmits(['close'])
+onMounted(() => {
+  document.body.style.overflow = 'hidden'
+  // console.log('mount')
+})
+onUnmounted(() => {
+  document.body.style.overflow = 'initial'
+  // console.log('UNmount')
+})
+</script>
+
+<template>
+  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]">
+    <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
+      <button>
+        <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
+      </button>
+      <slot></slot>
+    </div>
+  </div>
+</template>
