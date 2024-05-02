6e3976a6ea32d57fa2814a64c0cafe9c0c2f50dc
Sokilskill <vprovorotov@gmail.com>
2024-05-02 18:24:17 +0300

    added input image. save img base64

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import CrossIcon from '../icons/CrossIcon.vue'
+import InputImage from '../InputImage/InputImage.vue'
 const emit = defineEmits(['close'])
+
+const imageUrlBase64 = ref('')
+
 onMounted(() => {
   document.body.style.overflow = 'hidden'
   // console.log('mount')
@@ -22,6 +26,8 @@ onUnmounted(() => {
         <button>
           <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
         </button>
+        <img v-if="imageUrlBase64" :src="imageUrlBase64" alt="" />
+        <InputImage @uploaded="(base64) => (imageUrlBase64 = base64)" />
         <slot></slot>
       </div>
     </div>
