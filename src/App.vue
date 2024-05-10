4e21ea754bdbcf99b3a8c63298b3259d17ddd6c1
Sokilskill <vprovorotov@gmail.com>
2024-05-10 08:19:17 +0300

    add mapBox

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
-import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
-// import HomepageView from './views/HomepageView.vue'
-// import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
-import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
-
-const isOpen = ref(false)
-const closeModal = () => {
-  isOpen.value = false
-}
-const openModal = () => {
-  isOpen.value = true
-}
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { mapSetting } from './map/setting'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
   <!-- <HomepageView /> -->
-
-  <!-- <div class="bg-white h-screen w-[400px]">
-    <FavoritePlaces />
-  </div> -->
-  <button class="text-red-500 bg-amber-400 px-5 rounded-lg" @click="openModal">Open modal</button>
-  <RegistrationForm @submit="(e) => console.log(e)" />
-  <LoginForm @submit="console.log" />
-  <CreateNewPlaceModal :is-open="isOpen" @close="closeModal" @submit="console.log" />
+  <main class="flex h-screen">
+    <div class="bg-white h-screen w-[400px]">
+      <FavoritePlaces />
+    </div>
+    <div class="w-full h-full flex items-center justify-center text-2xl">
+      <MapboxMap
+        class="w-full h-full"
+        :center="[30.523333, 50.450001]"
+        :zoom="10"
+        :access-token="mapSetting.apiToken"
+        :map-style="mapSetting.style"
+      ></MapboxMap>
+    </div>
+  </main>
 </template>
---
+++ src/map/setting.js
@@ -0,0 +1,6 @@
+import { token } from './token'
+
+export const mapSetting = {
+  style: 'mapbox://styles/mapbox/streets-v12',
+  apiToken: token
+}
