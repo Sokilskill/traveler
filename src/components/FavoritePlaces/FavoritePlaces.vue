260c086904b5332614930df4664951a224ea6ab4
Sokilskill <vprovorotov@gmail.com>
2024-05-10 08:46:08 +0300

    rework FavoritePlaces

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,18 +1,13 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import IInput from '../IInput/IInput.vue'
 </script>
 
 <template>
   <div class="px-6">
     <h3 class="text-gray mb-4">Додані маркери</h3>
 
-    <div class="py-5">
-      <IInput label="Some label" />
-    </div>
-
     <FavoritePlace :key="n" v-for="n in 4" />
-    <IButton class="w-full mt-10">Додати маркер</IButton>
+    <IButton class="w-full mt-10" variant="gradient">Додати маркер</IButton>
   </div>
 </template>
