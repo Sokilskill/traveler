ba94cdbc4fae2e33fc4150ae19ee89bb83838f4a
Sokilskill <vprovorotov@gmail.com>
2024-05-10 16:46:29 +0300

    create Auth page

---
+++ src/views/AuthView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import IButton from '../components/IButton/IButton.vue'
+import BaseLayout from '../layouts/BaseLayout.vue'
+</script>
+
+<template>
+  <BaseLayout>
+    <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="map img" />
+    <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+    <h2>сторінка реєстрації</h2>
+    <IButton>Почати роботу</IButton>
+  </BaseLayout>
+</template>
