a0dcdae4c1280b58cf38a81f9d552742937dad88
Sokilskill <vprovorotov@gmail.com>
2024-05-12 21:11:15 +0300

    add marker on click

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -35,6 +35,7 @@ const favoritePlaces = ref([])
 
 const activeId = ref(null)
 const map = ref(null)
+const mapMarkerLngLat = ref(null)
 
 const changeActiveId = (id) => {
   activeId.value = id
@@ -44,6 +45,10 @@ const changePlace = (id) => {
   const { lngLat } = favoritePlaces.value.find((place) => place.id === id)
   changeActiveId(id)
   map.value.flyTo({ center: lngLat, zoom: 12 })
+}
+
+const handleMapClick = ({ lngLat }) => {
+  mapMarkerLngLat.value = [lngLat.lng, lngLat.lat]
 }
 
 onMounted(async () => {
@@ -64,9 +69,19 @@ onMounted(async () => {
         :zoom="10"
         :access-token="mapSetting.apiToken"
         :map-style="mapSetting.style"
+        @mb-click="handleMapClick"
         @mb-created="(mapInstance) => (map = mapInstance)"
       >
-        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
+        <MapboxMarker v-if="mapMarkerLngLat" :lngLat="mapMarkerLngLat" anchor="bottom">
+          <MarkerIcon class="h-8 w-8" />
+        </MapboxMarker>
+
+        <MapboxMarker
+          v-for="place in favoritePlaces"
+          :key="place.id"
+          :lngLat="place.lngLat"
+          anchor="bottom"
+        >
           <button @click="changeActiveId(place.id)">
             <MarkerIcon class="h-8 w-8" />
           </button>
