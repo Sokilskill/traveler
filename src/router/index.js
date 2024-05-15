9475d25cfe2d568a1db8617edcc483a8270efa33
Sokilskill <vprovorotov@gmail.com>
2024-05-15 18:12:00 +0300

    add logout, add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { authService } from '@/api/authService'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const HomepagePage = () => import('../views/HomepageView.vue')
@@ -9,16 +10,17 @@ const RegistrationPage = () => import('../views/RegistrationView.vue')
 const routes = [
   {
     path: '/',
-    component: GreetingPage
+    component: GreetingPage,
+    name: 'greeting'
   },
-  { path: '/map', component: HomepagePage },
+  { path: '/map', component: HomepagePage, name: 'homepage' },
   {
     path: '/auth',
     component: AuthPage,
     redirect: '/auth/login',
     children: [
-      { path: 'login', component: LoginPage },
-      { path: 'registration', component: RegistrationPage }
+      { path: 'login', component: LoginPage, name: 'login' },
+      { path: 'registration', component: RegistrationPage, name: registration }
     ]
   }
 ]
@@ -26,4 +28,17 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const authRoutes = ['login', 'registration']
+  const { name } = to
+
+  if (authService.isLoggedIn() && authRoutes.includes(name)) {
+    next({ name: 'homepage' })
+  } else if (!authRoutes.includes(name) && !authService.isLoggedIn()) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
